366217aafff7988e7b1fea35a2c1e387c338f4e0
Abel Fabian <Abelperez218@gmail.com>
2023-03-15 13:55:42 -0300

    Proyecto terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CafesComponent } from './components/cafes/cafes.component';
+import { SucursalesComponent } from './components/sucursales/sucursales.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    CafesComponent,
+    SucursalesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
